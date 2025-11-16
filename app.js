d0d67becd45e1858eb921ae7cdc440a3410c44f2
mchkaaa bc2025-6 <sdolosecka@gmail.com>
2025-11-16 15:57:34 +0200

    Додала commander.js, superagent, nodemon і оновила app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("Hello from backend course!");
+const { Command } = require('commander');
+const program = new Command();
+
+program
+  .version('1.0.0')
+  .description('Backend Course 2025-6 Application')
+  .option('-n, --name <name>', 'your name')
+  .parse(process.argv);
+
+const options = program.opts();
+
+if (options.name) {
+  console.log(`Hello, ${options.name}!`);
+} else {
+  console.log('Hello from backend course! Use --name to specify your name.');
+}console.log("Hello from backend course!");
